e633f4c384f6b161ccbb34ad26d241b3a97efa6d
Jason <Jason.patino94@gmail.com>
2021-10-29 10:46:21 -0400

    leetcode first problem more efficient way

--- LeetCode1/problem1-TwoSum.js
+++ LeetCode1/problem1-TwoSum.js
@@ -1,17 +1,28 @@
-const twoSum = (nums, target) => {
-    const arr = []
-    for (let i = 0; i < nums.length; i++)
-        for (let j = i+1; j < nums.length; j++){
-            let first = nums[i]
-            let second = nums[j]
+// const twoSum = (nums, target) => {
+//     const arr = []
+//     for (let i = 0; i < nums.length; i++)
+//         for (let j = i+1; j < nums.length; j++){
+//             let first = nums[i]
+//             let second = nums[j]
 
-            if (first + second === target){
-             arr.push(i)
-             arr.push(j)
-                console.log(arr)
-            }
-        }
+//             if (first + second === target){
+//              arr.push(i)
+//              arr.push(j)
+//                 console.log(arr)
+//             }
+//         }
+// }
+
+const twoSum = (nums, target) => {
+    let stored = {}
+
+    for(let [index, num] of nums.entries()){
+        if (stored[num] !== undefined) console.log([stored[num], index])
+        
+
+        stored[target - num] = index
+    }
 }
 
-twoSum([3,2,4], 6)
+twoSum([3,2,4,8,2], 6)
 //output = [0,1]
